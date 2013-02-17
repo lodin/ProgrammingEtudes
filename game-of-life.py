2b06b4225dfcd8ee5e4461552e88bc28212d5f41
Louie Dinh <LouieDinh@gmail.com>
2013-02-17 11:38:10 -0800

    Add error handling for scrolling off screen. Add trajectories

--- game-of-life.py
+++ game-of-life.py
@@ -1,14 +1,25 @@
+"""
+TODOS:
+    - Add a draw screen function to print out statistics
+    - Add infinite scroll
+    - Do trajectory modifications (clear trajectory)
+    - Actually write the game of life stuff
+"""
 import curses
 from collections import namedtuple
 
 Point = namedtuple("Point", ["row", "col"])
 Vector = namedtuple("Vector", ["row", "col"])
 
+CELL_CHAR = ord("o")
+
 class Command:
     """
     Stores the valid user input commands
     """
     MOVE_KEYS = ['KEY_LEFT', 'KEY_RIGHT', 'KEY_UP', 'KEY_DOWN']
+    INSERT_KEY = ' '
+    CLEAR_TRAJECTORY_KEY = '\n'
     
     @staticmethod
     def cursor_transform_for_command(key):
@@ -35,6 +46,7 @@ class GameOfLife:
     def __init__(self, stdscr):
         self.stdscr = stdscr
         origin = Point(row=0, col=0)
+        self.insert_tranjectory = Vector(row=0, col=0)
         self.start()
 
     def get_cursor_position(self) -> Point:
@@ -50,6 +62,7 @@ class GameOfLife:
             input_key = self.stdscr.getkey()
             continue_execution = self.dispatch_user_command(input_key)
 
+
     def dispatch_user_command(self, input_key):
         """
         Takes a user input and decides what to do about it
@@ -58,8 +71,20 @@ class GameOfLife:
             return False
         elif input_key in Command.MOVE_KEYS:
             transform = Command.cursor_transform_for_command(input_key)
+            self.insert_tranjectory = transform
             pos = self.get_cursor_position()
-            self.stdscr.move(pos.row + transform.row, pos.col + transform.col)
+            try:
+                self.stdscr.move(pos.row + transform.row, pos.col + transform.col)
+            except curses.error as e:
+                # We're probably just trying to move off the side of the screen
+                pass
+        elif input_key == Command.INSERT_KEY:
+            pos = self.get_cursor_position()
+            traj = self.insert_tranjectory
+            self.stdscr.addch(CELL_CHAR)
+            self.stdscr.move(pos.row + traj.row, pos.col + traj.col)
+        elif input_key == Command.CLEAR_TRAJECTORY_KEY:
+            self.insert_trajectory = Vector(row=0, col=0)
         else:
             self.stdscr.addstr(input_key)
 
