87c918a4da9d46835624a710f1cd96cef66e38d4
Louie Dinh <LouieDinh@gmail.com>
2013-02-18 21:03:27 -0800

    Change cell insertion to modal paradigm

--- game-of-life.py
+++ game-of-life.py
@@ -18,8 +18,7 @@ class Command:
     Stores the valid user input commands
     """
     MOVE_KEYS = ['KEY_LEFT', 'KEY_RIGHT', 'KEY_UP', 'KEY_DOWN']
-    INSERT_KEY = ' '
-    CLEAR_TRAJECTORY_KEY = '\n'
+    TOGGLE_INSERT_MODE_KEY = ' '
     
     @staticmethod
     def cursor_transform_for_command(key):
@@ -37,6 +36,10 @@ class Command:
         else:
             raise ValueError("{} is not a valid key".format(key))
 
+class Mode:
+    INSERT = 1
+    NORMAL = 2
+
 
 class GameOfLife:
     """
@@ -45,6 +48,7 @@ class GameOfLife:
 
     def __init__(self, stdscr):
         self.stdscr = stdscr
+        self.mode = Mode.NORMAL
         origin = Point(row=0, col=0)
         self.insert_tranjectory = Vector(row=0, col=0)
         self.start()
@@ -62,7 +66,6 @@ class GameOfLife:
             input_key = self.stdscr.getkey()
             continue_execution = self.dispatch_user_command(input_key)
 
-
     def dispatch_user_command(self, input_key):
         """
         Takes a user input and decides what to do about it
@@ -71,26 +74,30 @@ class GameOfLife:
             return False
         elif input_key in Command.MOVE_KEYS:
             transform = Command.cursor_transform_for_command(input_key)
-            self.insert_tranjectory = transform
             pos = self.get_cursor_position()
             try:
                 self.stdscr.move(pos.row + transform.row, pos.col + transform.col)
             except curses.error as e:
                 # We're probably just trying to move off the side of the screen
                 pass
-        elif input_key == Command.INSERT_KEY:
-            pos = self.get_cursor_position()
-            traj = self.insert_tranjectory
-            self.stdscr.addch(CELL_CHAR)
-            self.stdscr.move(pos.row + traj.row, pos.col + traj.col)
-        elif input_key == Command.CLEAR_TRAJECTORY_KEY:
-            self.insert_trajectory = Vector(row=0, col=0)
+            if self.mode == Mode.INSERT:
+                pos = self.get_cursor_position()
+                self.stdscr.addch(CELL_CHAR)
+                self.stdscr.move(pos.row, pos.col)
+            else:
+                pass
+        elif input_key == Command.TOGGLE_INSERT_MODE_KEY:
+            if self.mode == Mode.INSERT:
+                self.mode = Mode.NORMAL
+            else:
+                self.mode = Mode.INSERT
+                pos = self.get_cursor_position()
+                self.stdscr.addch(CELL_CHAR)
+                self.stdscr.move(pos.row, pos.col)
         else:
             self.stdscr.addstr(input_key)
 
         return True
-
-
 
 if __name__ == '__main__':
     curses.wrapper(GameOfLife)
