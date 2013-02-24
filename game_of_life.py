1143dd77c5494a10a72f2648d8eee4b2aac8ccfc
Louie Dinh <LouieDinh@gmail.com>
2013-02-23 18:38:07 -0800

    Refactor. Move screen into it's own class

--- game_of_life.py
+++ game_of_life.py
@@ -23,10 +23,11 @@ class Command:
     Stores the valid user input commands
     """
     MOVE_KEYS = ['KEY_LEFT', 'KEY_RIGHT', 'KEY_UP', 'KEY_DOWN']
-    TOGGLE_INSERT_MODE_KEY = ' '
+    TOGGLE_INSERT_MODE_KEY = 't'
     INSERT_CELL= 'c'
     DELETE_CELL = 'x'
     QUIT = 'q'
+    REFRESH = 'r'
     
     @staticmethod
     def cursor_transform_for_command(key):
@@ -61,6 +62,10 @@ class Board:
     def cell_at(self, point):
         return self.cell_positions.get(point, False)
 
+    def get_cells(self):
+        return self.cell_positions.keys()
+    cells = property(fget=get_cells)
+
     def add_cell(self, point) -> bool:
         """
         Add a cell at <point> :: Point.
@@ -94,20 +99,9 @@ class Board:
     def step(self):
         pass
 
-
-class GameOfLife:
-    """
-    An instance of the game of life.
-    """
+class Screen:
     def __init__(self, stdscr):
-        # Do initializations
         self.stdscr = stdscr
-        self.mode = Mode.NORMAL
-        origin = Point(row=0, col=0)
-        self.insert_tranjectory = Vector(row=0, col=0)
-        self.board = Board()
-        # Start the game
-        self.start()
 
     # Declare cursor_position as a property
     def get_cursor_position(self) -> Point:
@@ -115,37 +109,10 @@ class GameOfLife:
         return Point(row=row, col=column)
     cursor_position = property(fget=get_cursor_position)
 
-    def start(self):
-        """
-        Starts the Game of Life
-        """
-        continue_execution = True
-        while continue_execution:
-            input_key = self.stdscr.getkey()
-            continue_execution = self.dispatch_user_command(input_key)
+    def get_input_key(self):
+        return self.stdscr.getkey()
 
-    def dispatch_user_command(self, input_key):
-        """
-        Takes a user input and decides what to do about it
-        """
-        if input_key == Command.QUIT:
-            return False
-        elif input_key in Command.MOVE_KEYS:
-            transform = Command.cursor_transform_for_command(input_key)
-            if self.mode == Mode.INSERT: self.add_cell(self.cursor_position)
-            self.move(transform.row, transform.col)
-        elif input_key == Command.INSERT_CELL:
-            self.add_cell(self.cursor_position)
-        elif input_key == Command.DELETE_CELL:
-            self.remove_cell(self.cursor_position)
-        elif input_key == Command.TOGGLE_INSERT_MODE_KEY:
-            self.mode = Mode.INSERT if self.mode == Mode.NORMAL else Mode.NORMAL
-        else:
-            pass
-
-        return True
-
-    def move(self, row_delta, col_delta):
+    def move_cursor(self, row_delta, col_delta):
         """ 
         Move the cursor
         """
@@ -157,25 +124,79 @@ class GameOfLife:
 
         return True
 
-    def add_cell(self, position):
-        # Insert into our board
-        self.board.add_cell(position)
-        # Update the display
+    def add_cell(self, point):
         old_position = self.cursor_position
-        self.stdscr.move(position.row, position.col)
+        self.stdscr.move(point.row, point.col)
         self.stdscr.addch(CELL_CHAR)
         self.stdscr.move(old_position.row, old_position.col)
 
-    def remove_cell(self, position):
-        # Remove from our board
-        self.board.remove_cell(position)
-        # Update the display
+    def remove_cell(self, point):
         old_position = self.cursor_position
-        self.stdscr.move(position.row, position.col)
+        self.stdscr.move(point.row, point.col)
         self.stdscr.addch(BLANK_CHAR)
         self.stdscr.move(old_position.row, old_position.col)
 
+    def clear(self):
+        self.stdscr.erase()
 
+class GameOfLife:
+    """
+    An instance of the game of life.
+    """
+    def __init__(self, stdscr):
+        # Do initializations
+        self.screen = Screen(stdscr)
+        self.board = Board([Point(1,1),Point(2,2)])
+        self.mode = Mode.NORMAL
+        self.origin = Point(row=0, col=0)
+        # Start the game
+        self.start()
+
+    def start(self):
+        """
+        Starts the Game of Life
+        """
+        self.refresh_screen()
+        continue_execution = True
+        while continue_execution:
+            input_key = self.screen.get_input_key()
+            continue_execution = self.dispatch_user_command(input_key)
+
+    def dispatch_user_command(self, input_key):
+        """
+        Takes a user input and decides what to do about it
+        """
+        if input_key == Command.QUIT:
+            return False
+        elif input_key in Command.MOVE_KEYS:
+            transform = Command.cursor_transform_for_command(input_key)
+            if self.mode == Mode.INSERT: self.add_cell(self.screen.cursor_position)
+            self.screen.move_cursor(row_delta=transform.row, col_delta=transform.col)
+        elif input_key == Command.INSERT_CELL:
+            self.add_cell(self.screen.cursor_position)
+        elif input_key == Command.DELETE_CELL:
+            self.remove_cell(self.screen.cursor_position)
+        elif input_key == Command.REFRESH:
+            self.screen.redraw()
+        elif input_key == Command.TOGGLE_INSERT_MODE_KEY:
+            self.mode = Mode.INSERT if self.mode == Mode.NORMAL else Mode.NORMAL
+        else:
+            pass
+
+        return True
+
+    def add_cell(self, position):
+        self.board.add_cell(position)
+        self.screen.add_cell(position)
+
+    def remove_cell(self, position):
+        self.board.remove_cell(position)
+        self.screen.remove_cell(position)
+
+    def refresh_screen(self):
+        self.screen.clear()
+        for point in self.board.cells:
+            self.screen.add_cell(point)
 
 if __name__ == '__main__':
     curses.wrapper(GameOfLife)
